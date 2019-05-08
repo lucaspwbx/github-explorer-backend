c3c7963d3d109e94691b15b590e536c4ce271169
Lucas Weiblen <lucas.weiblen@gmail.com>
2019-05-08 20:13:36 -0300

    first commit

---
+++ db.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	userPg   = "lucas"
+	password = "teste"
+	dbname   = "foo_dev"
+)
+
+type project struct {
+	Id          int
+	Name        string
+	Description string
+	Author      string
+	Language    string
+	Url         string
+}
+
+type user struct {
+	Id       int
+	Username string
+	Password string
+	Email    string
+}
+
+func main() {
+	connString := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
+		host, port, userPg, password, dbname)
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected")
+
+	var projects []project
+	rows, err := db.Query(`
+		SELECT id, name, description, author, language, url from projects
+	`)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		project := project{}
+		err := rows.Scan(
+			&project.Id,
+			&project.Name,
+			&project.Description,
+			&project.Author,
+			&project.Language,
+			&project.Url)
+		if err != nil {
+			panic(err)
+		}
+		projects = append(projects, project)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(projects)
+
+	var users []user
+	rows, err = db.Query(`
+		SELECT id, username, password, email from users`)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		us := user{}
+		err = rows.Scan(
+			&us.Id,
+			&us.Username,
+			&us.Password,
+			&us.Email)
+		if err != nil {
+			panic(err)
+		}
+		users = append(users, us)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(users)
+}
---
+++ go.mod
@@ -0,0 +1,20 @@
+module teste
+
+go 1.12
+
+require (
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/lib/pq v1.1.1
+	github.com/neelance/parallel v0.0.0-20160708114440-4de9ce63d14c // indirect
+	github.com/opentracing/basictracer-go v1.0.0 // indirect
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v0.9.2 // indirect
+	github.com/slimsag/godocmd v0.0.0-20161025000126-a1005ad29fe3 // indirect
+	github.com/sourcegraph/ctxvfs v0.0.0-20180418081416-2b65f1b1ea81 // indirect
+	github.com/sourcegraph/go-langserver v2.0.0+incompatible // indirect
+	github.com/sourcegraph/jsonrpc2 v0.0.0-20190106185902-35a74f039c6a // indirect
+	golang.org/x/net v0.0.0-20190503192946-f4e77d36d62c // indirect
+	golang.org/x/tools v0.0.0-20190508150211-cf84161cff3f // indirect
+)
