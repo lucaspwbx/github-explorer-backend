9468234f1d256a2834817bf7f27d50b093c49fdb
Lucas Weiblen <lucasweiblen@gmail.com>
2019-05-18 19:51:51 -0300

    clean up a little bit more

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ const (
 )
 
 var (
-	config db.Config
+	config *db.Config
 )
 
 func addNewUserHandler(c echo.Context) error {
@@ -66,52 +66,56 @@ func logoutUserHandler(c echo.Context) error {
 	return nil
 }
 
+func getBookmarkedProjectsHandler(c echo.Context) error {
+	userId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return err
+	}
+	projects := db.FetchUserBookmarkedProjects(config.Connection(), userId)
+	if projects != nil {
+		return c.JSON(http.StatusOK, projects)
+	}
+	return c.JSON(http.StatusOK, projects)
+}
+
+func addBookmarkedProjectHandler(c echo.Context) error {
+	p := &db.Project{}
+	userId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return err
+	}
+	if err := c.Bind(p); err != nil {
+		return err
+	}
+	projectId, err := db.ProjectExists(config.Connection(), p.Name, p.Author, p.Language)
+	if err != nil {
+		newProjectId, err := db.AddProject(config.Connection(), p)
+		if err != nil {
+			return err
+		}
+		_, err = db.BookmarkProject(config.Connection(), userId, newProjectId)
+		if err != nil {
+			log.Println("Problems bookmarking project 1")
+			return c.JSON(http.StatusBadRequest, "Failed to bookmark project -> 1")
+		}
+		return c.JSON(http.StatusOK, "Bookmarked project 1")
+	}
+	_, err = db.BookmarkProject(config.Connection(), userId, projectId)
+	if err != nil {
+		log.Println("Problems bookmarking project 2")
+		return c.JSON(http.StatusBadRequest, "Failed to bookmark project -> 2")
+	}
+	return c.JSON(http.StatusOK, "bookmarked project 2")
+}
+
 func main() {
-	config := db.NewConfig(host, port, userPg, password, dbname)
+	config = db.NewConfig(host, port, userPg, password, dbname)
 	e := echo.New()
 	e.Use(middleware.CORS())
 	e.POST("/users", addNewUserHandler)
 	e.POST("/users/login", loginUserHandler)
 	e.POST("/users/logout", logoutUserHandler)
-	e.GET("/users/:id/bookmarked_projects", func(c echo.Context) error {
-		userId, err := strconv.Atoi(c.Param("id"))
-		if err != nil {
-			return err
-		}
-		projects := db.FetchUserBookmarkedProjects(config.Connection(), userId)
-		if projects != nil {
-			return c.JSON(http.StatusOK, projects)
-		}
-		return c.JSON(http.StatusOK, projects)
-	})
-	e.POST("/users/:id/bookmarked_projects", func(c echo.Context) error {
-		p := &db.Project{}
-		userId, err := strconv.Atoi(c.Param("id"))
-		if err != nil {
-			return err
-		}
-		if err := c.Bind(p); err != nil {
-			return err
-		}
-		projectId, err := db.ProjectExists(config.Connection(), p.Name, p.Author, p.Language)
-		if err != nil {
-			newProjectId, err := db.AddProject(config.Connection(), p)
-			if err != nil {
-				return err
-			}
-			_, err = db.BookmarkProject(config.Connection(), userId, newProjectId)
-			if err != nil {
-				log.Println("Problems bookmarking project 1")
-				return c.JSON(http.StatusBadRequest, "Failed to bookmark project -> 1")
-			}
-			return c.JSON(http.StatusOK, "Bookmarked project 1")
-		}
-		_, err = db.BookmarkProject(config.Connection(), userId, projectId)
-		if err != nil {
-			log.Println("Problems bookmarking project 2")
-			return c.JSON(http.StatusBadRequest, "Failed to bookmark project -> 2")
-		}
-		return c.JSON(http.StatusOK, "bookmarked project 2")
-	})
+	e.GET("/users/:id/bookmarked_projects", getBookmarkedProjectsHandler)
+	e.POST("/users/:id/bookmarked_projects", addBookmarkedProjectHandler)
 	e.Logger.Fatal(e.Start(":1323"))
 }
